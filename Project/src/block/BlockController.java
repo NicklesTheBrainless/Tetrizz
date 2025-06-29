63f98d40063bf99269755b3c102481b8e13373d3
NicklesTheBrainless <NicoNickles8@gmail.com>
2025-06-29 17:48:16 +0200

    updated BlockController

--- Project/src/block/BlockController.java
+++ Project/src/block/BlockController.java
@@ -1,10 +1,22 @@
 package block;
 
+import base.panel.GamePanel;
 import utils.GameObject;
+import utils.Utils;
 
 import java.awt.*;
 
+import static base.setting.Settings.*;
+
 public class BlockController implements GameObject {
+
+    GamePanel gp;
+
+    Block currentBlock;
+
+    public BlockController(GamePanel gp) {
+        this.gp = gp;
+    }
 
     @Override
     public void update(double delta) {
@@ -14,5 +26,29 @@ public class BlockController implements GameObject {
     @Override
     public void draw(Graphics2D g2) {
 
+    }
+
+
+
+    void fallBlock() {
+
+        currentBlock.x += 1;
+
+        for (int partY = 0; partY < MAX_BLOCK_HEIGHT; partY++) {
+            for (int partX = 0; partX < MAX_BLOCK_WIDTH; partX++) {
+
+                if (currentBlock.getPart(partX, partY) == 0)
+                    continue;
+
+                // TODO: do shit here yk
+            }
+        }
+    }
+
+    void spawnBlock() {
+
+        BlockType[] allTypes = BlockType.values();
+        BlockType randomType = allTypes[Utils.random.nextInt(allTypes.length)];
+        currentBlock = new Block((GRID_WIDTH + MAX_BLOCK_WIDTH) / 2, MAX_BLOCK_HEIGHT / 2, randomType);
     }
 }
